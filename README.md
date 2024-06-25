22c3baaea2f815231ff310dbd958ec51dcc8c539
Michael Muller <mwmuller94@gmail.com>
2024-06-24 20:28:04 -0400

    Updating ptlnx env script, adding tftp to docker image

--- README.md
+++ README.md
@@ -12,13 +12,15 @@ Before you can run the required scripts and tools you'll need to reopen your env
 #
 ### Note:
     !!!This process needs to be done each time you want to use the devcontainer!!!
-1) In the bottom left of vc-code you can click the "><" -> Connect to WSL -> Select the ```petalinx-wsl``` folder <br />
+1) In the bottom left of vc-code you can click the "><" -> ```Connect to WSL``` -> Select the ```petalinx-wsl``` folder <br />
     1a) Alternativly ```ctrl + shift + p``` -> ```Connect to WSL```
 
 2) In the bottom left of vc-code you can click the "><" -> ```Dev-container > Reopen in container``` <br />
     2b) ```ctrl + shift + p``` -> ```Dev-container: Reopen in container```
 
-3) To open a new ```dash``` terminal you can use the key-bind ```ctrl + shift + ```
+- To open a new ```bash``` terminal you can use the key-bind ```ctrl + shift + ` ```
 
 ## Petalinux setup **(Requires Petalinux-installer)**
-1) Run the ```first-time-ptlnx-setup.sh``` to install the required petalinux dependencies. **This will take some time ~20 minutes**
+1) Run the ```./first-time-ptlnx-setup.sh``` to setup the petalinux environment
+    - To include your own version of petalinux, pass the file name as an arg: ```./first-time-ptlnx-setup.sh petalinux-v20xx.x-installer.run```
+2) 
--- first-time-ptlnx-setup.sh
+++ first-time-ptlnx-setup.sh
@@ -1,21 +1,42 @@
 #!/bin/bash
 
-# Runningthe petalinux sh from support.xilinx.com/s/article/73296
-cd scripts/
-
-sudo ./plnx-env-setup.sh
-
+# Running the petalinux sh from support.xilinx.com/s/article/73296
+CREATE_INSTALL="true"
+PETALINUX_INSTALLER=${1:-"petalinux-v2019.2-final-installer.run"}
 PTLNX_FOLDER="petalinux-tools"
-mkdir ${PTLNX_FOLDER}
+if [[ -d "$PTLNX_FOLDER" ]]; then
+    echo "$PTLNX_FOLDER Folder alredy created do you want to delete it and re-install? [y/n]"
+    read SELECTION
+    if [[ "$SELECTION" == "y" ]]; then
+        CREATE_INSTALL="true"
+        echo "Removing tools folder"
+        rm -rf "$PTLNX_FOLDER"
+    else
+        echo "Exitting..."
+        exit 1
+    fi
+fi
+
+mkdir ${PTLNX_FOLDER} 
 if [[ -d "$PTLNX_FOLDER" ]]; then
     echo "$PTLNX_FOLDER created..."
     cd petalinux-tools
 else
-    echo "Filed to create ${PTLNX_FOLDER}"
+    echo "Failed to create ${PTLNX_FOLDER}"
     exit 1
 fi
 
-sudo ../petalinux-v2019.2-final-installer.run 
+PETALINUX_CHMOD_X="$(test -x $PETALINUX_INSTALLER && echo "true" || echo "false")"
+# No need to chmod if bit already set
+if [[ "${PETALINUX_CHMOD_X}" == "false" ]]; then
+    chmod +x ../${PETALINUX_INSTALLER}
+    echo -e "\e[33mSetting executable bit for petalinux installer.\n\e[0m"
+else
+    echo "Installer executable bit already set."
+fi
+
+# Shell is currently in petalinux-tools folder.
+./../"$PETALINUX_INSTALLER" "$PTLNX_FOLDER"
 
 source ./settings.sh
 
