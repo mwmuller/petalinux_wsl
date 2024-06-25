d86db2ddeb05149f42b8c6a2fcf5a6cddb9b234e
Michael Muller <mwmuller94@gmail.com>
2024-06-25 14:58:08 -0400

    Adding packages for EULA and petalinux installation. Updating README

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-## WSL/Docker Installation
+## WSL/Docker Installation Pre-Requisites **(Must be done first)**
 1) Open the wsl-localenv folder to install WSL and Docker
 
 
@@ -13,14 +13,26 @@ Before you can run the required scripts and tools you'll need to reopen your env
 ### Note:
     !!!This process needs to be done each time you want to use the devcontainer!!!
 1) In the bottom left of vc-code you can click the "><" -> ```Connect to WSL``` -> Select the ```petalinx-wsl``` folder <br />
-    1a) Alternativly ```ctrl + shift + p``` -> ```Connect to WSL```
+    - Alternativly ```ctrl + shift + p``` -> ```Connect to WSL```
 
 2) In the bottom left of vc-code you can click the "><" -> ```Dev-container > Reopen in container``` <br />
-    2b) ```ctrl + shift + p``` -> ```Dev-container: Reopen in container```
+    - ```ctrl + shift + p``` -> ```Dev-container: Reopen in container```
 
 - To open a new ```bash``` terminal you can use the key-bind ```ctrl + shift + ` ```
 
 ## Petalinux setup **(Requires Petalinux-installer)**
 1) Run the ```./first-time-ptlnx-setup.sh``` to setup the petalinux environment
-    - To include your own version of petalinux, pass the file name as an arg: ```./first-time-ptlnx-setup.sh petalinux-v20xx.x-installer.run```
-2) 
+
+**(OPTIONAL)**
+- You can include your own ```version``` of petalinux with ```-v```: ```./first-time-ptlnx-setup.sh -v petalinux-v20xx.x-installer.run```
+- The default directory for the petalinux installation is ```${WorkingDirectory}/petalinux-tools``` <br />
+You can update this with ```-p``` and provide the desired path: ```./first-time-ptlnx-setup.sh -p <newPath>```
+### REQUIRED ###
+#
+There are ```2``` instances during installation where user input is required: 
+- Checking Petalinux installer integrity
+- Petalinux installation
+**Enter ```q``` to exit ```eula.txt``` when it pops up and type ```y```**
+
+### Troubleshooting/Help
+1) Here is the link to the 2019.2 Petalinux installation guide: https://docs.amd.com/v/u/2019.2-English/ug1144-petalinux-tools-reference-guide
--- first-time-ptlnx-setup.sh
+++ first-time-ptlnx-setup.sh
@@ -2,41 +2,53 @@
 
 # Running the petalinux sh from support.xilinx.com/s/article/73296
 CREATE_INSTALL="true"
-PETALINUX_INSTALLER=${1:-"petalinux-v2019.2-final-installer.run"}
-PTLNX_FOLDER="petalinux-tools"
-if [[ -d "$PTLNX_FOLDER" ]]; then
-    echo "$PTLNX_FOLDER Folder alredy created do you want to delete it and re-install? [y/n]"
+ROOT_DIR=$(pwd)
+# Default values
+PETALINUX_INSTALLER="petalinux-v2019.2-final-installer.run"
+PETALINUX="$ROOT_DIR/petalinux-tools"
+# Parse command-line options using getopts
+while getopts ":v:p:" opt; do
+  case ${opt} in
+    v ) PETALINUX_INSTALLER=$OPTARG ;;
+    p ) PETALINUX=$OPTARG ;;
+    \? ) echo "Usage: ./first-time-ptlnx-setup [-i installer] [-p folder]"
+      exit 1 ;;
+  esac
+done
+
+if [[ -d "$PETALINUX" ]]; then
+    echo "$PETALINUX Folder alredy created do you want to delete it and re-install? [y/n]"
     read SELECTION
     if [[ "$SELECTION" == "y" ]]; then
         CREATE_INSTALL="true"
         echo "Removing tools folder"
-        rm -rf "$PTLNX_FOLDER"
+        rm -rf "$PETALINUX"
     else
         echo "Exitting..."
         exit 1
     fi
 fi
 
-mkdir ${PTLNX_FOLDER} 
-if [[ -d "$PTLNX_FOLDER" ]]; then
-    echo "$PTLNX_FOLDER created..."
-    cd petalinux-tools
+mkdir ${PETALINUX} 
+if [[ -d "$PETALINUX" ]]; then
+    echo "$PETALINUX created..."
 else
-    echo "Failed to create ${PTLNX_FOLDER}"
+    echo "Failed to create ${PETALINUX}"
     exit 1
 fi
 
 PETALINUX_CHMOD_X="$(test -x $PETALINUX_INSTALLER && echo "true" || echo "false")"
 # No need to chmod if bit already set
 if [[ "${PETALINUX_CHMOD_X}" == "false" ]]; then
-    chmod +x ../${PETALINUX_INSTALLER}
+    chmod +x $ROOT_DIR/${PETALINUX_INSTALLER}
     echo -e "\e[33mSetting executable bit for petalinux installer.\n\e[0m"
 else
     echo "Installer executable bit already set."
 fi
 
 # Shell is currently in petalinux-tools folder.
-./../"$PETALINUX_INSTALLER" "$PTLNX_FOLDER"
+export PETALINUX # export for script env
+"$ROOT_DIR"/"$PETALINUX_INSTALLER" "$PETALINUX"
 
-source ./settings.sh
+source ./"$PETALINUX"/settings.sh
 
